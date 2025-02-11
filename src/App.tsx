9e352f69d7401bbe21556ceb8d3388b029de87c2
VH-str <77taxman77@gmail.com>
2025-02-11 12:15:43 +0200

    369

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Item } from "./types/Item";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState<Array<object>>([]);
+  const [data, setData] = useState<Array<Item>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
   const [page, setPage] = useState<number>(1);
@@ -64,7 +64,7 @@ function App() {
     }
   };
 
-  const handleModalOpen = (elem: Record<string, unknown>): void => {
+  const handleModalOpen = (elem: Item): void => {
     if (isModalOpen) return;
     setCurrentElem(elem);
     setIsModalOpen(true);
